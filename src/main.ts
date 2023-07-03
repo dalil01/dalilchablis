559cd2b88b92ced41f7f267f07a133674980bf4f
Dalil CHABLIS <dalil.chablis@gmail.com>
2023-07-03 02:14:44 +0200

    Use vitePWA

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,5 @@ import "./styles.css";
 import { dcUIManager } from "./app/ui/dcUIManager";
 
 window.addEventListener("load", () => {
-	if ("serviceWorker" in navigator) {
-		(async () => {
-			const { default: serviceWorker } = await import("./service-worker.js");
-			navigator.serviceWorker.register(serviceWorker).then(function(registration) {
-				console.log("Service Worker successfully registered:", registration.scope);
-			}).catch(function(error) {
-				console.log("Service Worker registration failed:", error);
-			});
-		})();
-	}
-
 	dcUIManager.get(globalThis.document.body).start();
 });
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { viteStaticCopy } from "vite-plugin-static-copy";
 import gltfPlugin from 'vite-plugin-gltf';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default {
     base: "./",
@@ -12,7 +13,17 @@ export default {
     build: {
         emptyOutDir: true,
         outDir: "../dist",
-        assetsDir: "app"
+        assetsDir: "app",
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return 'vendor';
+                    }
+                },
+            },
+        },
     },
     plugins: [
         gltfPlugin(),
@@ -31,6 +42,58 @@ export default {
                     dest: "../dist/libs/draco/"
                 }
             ]
+        }),
+        VitePWA({
+            manifest: {
+                name: "Dalil CHABLIS",
+                short_name: "Dalil CHABLIS",
+                start_url: "https://dalilchablis.com/",
+                display: "standalone",
+                background_color: "#F7F7F7",
+                background_color_dark: "#242424",
+                theme_color: "#F7F7F7",
+                theme_color_dark: "#242424",
+                icons: [
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoWhite192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        media: "(prefers-color-scheme: light)"
+                    },
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoWhite196.png",
+                        sizes: "196x196",
+                        type: "image/png",
+                        purpose: "any maskable",
+                        media: "(prefers-color-scheme: light)"
+                    },
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoWhite512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        media: "(prefers-color-scheme: light)"
+                    },
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoBlack192.png",
+                        sizes: "192x192",
+                        type: "image/png",
+                        media: "(prefers-color-scheme: dark)"
+                    },
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoBlack196.png",
+                        sizes: "196x196",
+                        type: "image/png",
+                        purpose: "any maskable",
+                        media: "(prefers-color-scheme: dark)"
+                    },
+                    {
+                        src: "https://dalilchablis.com/assets/images/dcLogoBlack512.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                        media: "(prefers-color-scheme: dark)"
+                    }
+                ]
+            }
         })
     ]
 }
